3553e87b7a5d7f2a369c71aaafe80e2d2df4ea40
brianjboucher <brianb823@gmail.com>
2020-10-15 19:23:16 -0500

    Initial Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followThis;
+    Vector3 camOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camOffset = followThis.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = followThis.position - camOffset;
+    }
+}
---
+++ Assets/Scripts/MouseControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseControl : MonoBehaviour
+{
+    public float speed = 10.0f;
+    Vector3 goToPoint;
+    NavMeshAgent navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goToPoint = transform.position;
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mouse clicked");
+
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rhInfo;
+            int mouseMask = ~LayerMask.GetMask("PlayerCharacter");
+
+            if (Physics.Raycast(mouseRay, out rhInfo, 50.0f, mouseMask))
+            {
+                Debug.Log("Mouse ray hit:" + rhInfo.collider.gameObject.name + " at " + rhInfo.point);
+
+                goToPoint = rhInfo.point;
+                goToPoint.y = transform.position.y;
+
+                navMeshAgent.SetDestination(goToPoint);
+            }
+            else
+            {
+                Debug.Log("Mouse ray hit nothing");
+            }
+        }
+
+        //float distToPoint = Vector3.Distance(transform.position, goToPoint);
+        //if (distToPoint > 0.1f)
+        //{
+        //    transform.LookAt(goToPoint);
+        //    transform.position += transform.forward * Time.deltaTime * speed;
+        //}
+    }
+}
