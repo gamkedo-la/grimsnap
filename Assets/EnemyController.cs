ef634e6cbfa753a36f13e0c67b0688c1dbe76142
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2020-11-05 22:41:16 +0500

    Changed MouseControl.cs to PlayerController.cs. Click on Enemy to Attack: Indicator displays on top [WIP]. Added Enemy and Patrolling script (not working properly with navigation system).

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Transform[] patrolPoints;
+    public bool drawPatrolGizmos = false;
+    private int patrolIndex = 0;
+    
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, patrolPoints[patrolIndex].position) < 0.5f)
+        {
+            //navMeshAgent.SetDestination(patrolPoints[patrolIndex++].position);
+            patrolIndex++;
+
+            if (patrolIndex >= patrolPoints.Length)
+            {
+                patrolIndex = 0;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[patrolIndex].position,
+                speed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (drawPatrolGizmos)
+        {
+            for (int i = 0; i < patrolPoints.Length - 1; i++)
+            {
+                Debug.DrawLine(patrolPoints[i].position, patrolPoints[i + 1].position, Color.green);
+            }
+
+            if (patrolPoints.Length > 1)
+            {
+                Debug.DrawLine(patrolPoints[0].position, patrolPoints[patrolPoints.Length - 1].position, Color.green);
+            }
+        }
+    }
+}
