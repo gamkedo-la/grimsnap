67bbf88af41004643f2c1dafe9bc5e2ddb235ceb
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2020-11-08 18:31:48 +0500

    Integrated Goblin Enemy along with idle and walk animations. Improved Enemy Patrol and Attack on Enemy Indicator.

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -9,32 +9,71 @@ public class EnemyController : MonoBehaviour
     public float speed = 10.0f;
     public Transform[] patrolPoints;
     public bool drawPatrolGizmos = false;
-    private int patrolIndex = 0;
+    [Space]
+    public float restAfterMinDelay = 1.6f;
+    public float restAfterMaxDelay = 2.4f;
+    public float restMinTime = 0.4f;
+    public float restMaxTime = 1.2f;
+    [Space]
+    public Animator animator;
     
+    private int patrolIndex = 0;
+
     private NavMeshAgent navMeshAgent;
+    
+    private float restTimer = 0.0f;
+    private bool rest = true;
 
     void Start()
     {
         navMeshAgent = this.GetComponent<NavMeshAgent>();
+        restTimer = UnityEngine.Random.Range(restMinTime, restMaxTime);
     }
     
     void Update()
     {
-        if (Vector3.Distance(transform.position, patrolPoints[patrolIndex].position) < 0.5f)
+        if (!rest)
         {
-            //navMeshAgent.SetDestination(patrolPoints[patrolIndex++].position);
-            patrolIndex++;
-
-            if (patrolIndex >= patrolPoints.Length)
+            if (Vector3.Distance(transform.position, patrolPoints[patrolIndex].position) < 0.5f)
             {
-                patrolIndex = 0;
+                //navMeshAgent.SetDestination(patrolPoints[patrolIndex++].position);
+                patrolIndex++;
+
+                if (patrolIndex >= patrolPoints.Length)
+                {
+                    patrolIndex = 0;
+                }
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, patrolPoints[patrolIndex].position,
+                    speed * Time.deltaTime);
+                animator.SetBool("isWalking", true);
+
+                Vector3 targetDirection = patrolPoints[patrolIndex].position - transform.position;
+                float singleStep = speed * 4.0f * Time.deltaTime;
+                Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+                transform.rotation = Quaternion.LookRotation(newDirection);
             }
         }
         else
         {
-            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[patrolIndex].position,
-                speed * Time.deltaTime);
+            animator.SetBool("isWalking", false);
         }
+
+        if (restTimer <= 0.0f)
+        {
+            rest = !rest;
+            if (rest)
+                restTimer = UnityEngine.Random.Range(restMinTime, restMaxTime);
+            else
+                restTimer = UnityEngine.Random.Range(restAfterMinDelay, restAfterMaxDelay);
+        }
+        else
+        {
+            restTimer -= Time.deltaTime;
+        }
+        
     }
 
     private void OnDrawGizmos()
