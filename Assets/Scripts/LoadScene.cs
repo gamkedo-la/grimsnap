8717771d34fa1f203acf1e7087f86ba6efc2afc5
brianjboucher <brianb823@gmail.com>
2020-11-29 08:41:39 -0600

    Create Placeholder Win and Game Over screens

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void LoadWinScreenScene()
+    {
+        SceneManager.LoadScene("Win Screen");
+    }
+
+    public void LoadGameOverScreenScene()
+    {
+        SceneManager.LoadScene("Game Over Screen");
+    }
+}
