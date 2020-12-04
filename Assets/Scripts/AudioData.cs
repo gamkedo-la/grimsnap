bfb4e58d982321f3d58903fbe37d0562c545dab2
rocleemusic <48lee@cardinalmail.cua.edu>
2020-12-04 17:15:07 -0500

    Setting up Audio implementation

---
+++ Assets/Scripts/AudioData.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public enum AudioOutputGroup { Master, Music, PlayerSound, EnemySound, Ambience }
+
+[CreateAssetMenu(menuName = "GrimSnap/AudioData", fileName = "New AudioData.asset")]
+public class AudioData : ScriptableObject
+{
+    [SerializeField] List<AudioClip> Sounds = new List<AudioClip>();
+
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] AudioOutputGroup output;
+
+    [SerializeField] bool Loop = false;
+
+    [Range(-80, 0.0001f)]
+    [SerializeField] float Volume;
+
+    [Range(-24, 24)]
+    [SerializeField] float Pitch = 0f;
+
+    [Range(0f, 1f)]
+    [SerializeField] float SpatialBlend = 1f;
+
+    [Range(0, 5)]
+    [SerializeField] float RandomVolume = 0f;
+    [SerializeField] bool RandomizeVolume = false;
+
+    [Range(0, 12)]
+    [SerializeField] float RandomPitch = 0f;
+    [SerializeField] bool RandomizePitch = false;
+
+    #region AudioSource Parameters
+    public float GetVol()
+    {
+        if (RandomizeVolume == true)
+        {
+            return AudioFunctions.DbToLinear(Volume + GetRandomValueOffset(RandomVolume));
+        }
+        else
+        {
+            return AudioFunctions.DbToLinear(Volume);
+        }
+    }
+
+    private float GetRandomValueOffset(float value)
+    {
+        return Random.Range(-value, value);
+    }
+
+    public float GetPitch()
+    {
+        if (RandomizePitch == true)
+        {
+            return AudioFunctions.St2pitch(Pitch + GetRandomValueOffset(RandomPitch));
+        }
+        else
+        {
+            return AudioFunctions.St2pitch(Pitch);
+        }
+    }
+
+    public bool IsLooping() { return Loop; }
+    public float GetSpatialBlend() { return SpatialBlend; }
+
+    public AudioMixerGroup GetOutputGroup(GameObject calledBy)
+    {
+        string groupName;
+
+        switch (output)
+        {
+            case AudioOutputGroup.Master:
+                groupName = "Master";
+                break;
+            case AudioOutputGroup.Music:
+                groupName = "Music";
+                break;
+            case AudioOutputGroup.PlayerSound:
+                groupName = "PlayerSound";
+                break;
+            case AudioOutputGroup.EnemySound:
+                groupName = "EnemySound";
+                break;
+            case AudioOutputGroup.Ambience:
+                groupName = "Ambience";
+                break;
+            default:
+                groupName = "Master";
+                break;
+        }
+
+        if (mixer != null)
+        {
+            return mixer.FindMatchingGroups(groupName)[0];
+        }
+        else
+        {
+            Debug.LogError("Mixer Output Group could not be set by: " + calledBy.name);
+            return null;
+        }
+
+    }
+    #endregion
+
+    #region AudioClip Logic
+
+    public AudioClip GetClip(GameObject calledBy)
+    {
+        var numberOfClips = Sounds.Count;
+
+        if (numberOfClips == 0)
+        {
+            Debug.LogError("AudioData called by: " + calledBy.name + " does not contain any AudioClips.");
+            return null;
+        }
+
+        if (numberOfClips > 1)
+        {
+            return Sounds[Random.Range(0, numberOfClips)];
+        }
+        else
+        {
+            return Sounds[0];
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/AudioFunctions.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class AudioFunctions : MonoBehaviour
+{
+    /// <summary>
+    /// Converts Decibel Logarithmic scale float value into linear scale value from 0 to 1 for to feed game engine functions.
+    /// </summary>
+    /// <param name="dB"></param>
+    /// <returns>float value of the conversion</returns>
+    public static float DbToLinear(float dB)
+    {
+        if (dB > -80)
+        {
+            return Mathf.Clamp01(Mathf.Pow(10.0f, dB / 20.0f));
+        }
+        else
+            return 0;
+    }
+
+    /// <summary>
+    /// Converts linear scale slider value from .01 to 1 into corresponding Decibel log value for the mixer.
+    /// </summary>
+    /// <param name="linear"></param>
+    /// <returns>float value of the conversion</returns>
+    public static float LinearToDb(float linear)
+    {
+        if (linear > 0)
+        {
+            return Mathf.Clamp(20.0f * Mathf.Log10(linear), -80f, 0f);
+        }
+        else
+            return -80.0f;
+    }
+
+    /// <summary>
+    /// Static value representing one semi-tone
+    /// </summary>
+    private static float twelfthRootOfTwo = Mathf.Pow(2, 1.0f / 12);
+
+    /// <summary>
+    /// Converts semitones into pitchshift float value
+    /// </summary>
+    /// <param name="st"></param>
+    /// <returns>float value of the conversion</returns>
+    public static float St2pitch(float st)
+    {
+        return Mathf.Clamp(Mathf.Pow(twelfthRootOfTwo, st), 0f, 4f);
+    }
+
+    /// <summary>
+    /// Converts pitchshift float value into semitones
+    /// </summary>
+    /// <param name="pitch"></param>
+    /// <returns>float value of the conversion</returns>
+    public static float Pitch2st(float pitch)
+    {
+        return Mathf.Log(pitch, twelfthRootOfTwo);
+    }
+
+}
