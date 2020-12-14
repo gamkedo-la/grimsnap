1a60b4d762c0b267bc8fe1f1608bb5222544396a
rocleemusic <48lee@cardinalmail.cua.edu>
2020-12-14 12:57:34 -0500

    basic music plays on start and fades in

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.Events;
 public enum AudioState { Normal, Battle, GameOver }
 public class MusicManager : MonoBehaviour
 {
+    [Header("Music Manager Config")]
     public bool playStingers;
+    [SerializeField] AudioState currentAudioState;
+    internal AudioState heldAudioState;
+
+    [Header("Mixer Config")]
+    [SerializeField] AudioMixer mixer; //Todo, add Snapshot changes to Music Change Transitions
+    public AudioMixerSnapshot gameStartSnapshot;
+    public AudioMixerSnapshot normalMusicSnapshot;
 
     //public UnityAction<GameObject, AudioState> musicStateChange;
+    [Header("Music Events")]
     public UnityEvent playNormalMusic;
     public UnityEvent playBattleMusic;
     public UnityEvent playGameOverMusic;
     public UnityEvent playStinger;
 
-    [SerializeField] AudioState currentAudioState;
-    internal AudioState heldAudioState;
 
     void Start()
     {
         UpdateAudioState(AudioState.Normal);
+        MusicChange(currentAudioState);
+
+        if (normalMusicSnapshot != null)
+            normalMusicSnapshot.TransitionTo(2f);
+        else
+            Debug.LogError("Did you setup mixer in the music manager?");
     }
 
     void Update()
