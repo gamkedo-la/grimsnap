1d62c0dae8634bebb5baccda8ced00b40d497ea4
deepbluezen <deepbluezen@users.noreply.github.com>
2020-12-28 13:39:00 +0000

    Restored RepoPullReminder script but placed in Editor folder so builds will run

---
+++ Assets/Editor/RepoPullReminder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class RepoPullReminder : EditorWindow
+{
+    static RepoPullReminder()
+    {
+        if (!EditorApplication.isPlayingOrWillChangePlaymode) {
+            EditorApplication.update += RunOnce;
+        }
+    }
+
+    static void RunOnce(){
+        int windowWidth = 250;
+        int windowHeight = 100;
+
+        RepoPullReminder window = ScriptableObject.CreateInstance<RepoPullReminder>();
+        window.position = new Rect((Screen.width / 2) + (windowWidth / 2), Screen.height / 2 + (windowHeight / 2), windowWidth, windowHeight);
+        window.ShowPopup();
+        EditorApplication.update -= RunOnce;
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("Did you remember to pull the latest commits from the Git repo?", EditorStyles.wordWrappedLabel);
+        GUILayout.Space(30);
+        if (GUILayout.Button("Yes")){
+            this.Close();
+        } 
+    }
+}
